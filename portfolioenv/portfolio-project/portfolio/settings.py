000599f036d10e5c152186074b18691c5440ed5d
이정민 <asd45kj@gmail.com>
2019-12-14 12:22:46 +0900

    정민1차수정

--- portfolioenv/portfolio-project/portfolio/settings.py
+++ portfolioenv/portfolio-project/portfolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -33,6 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'friends.apps.FriendsConfig',
+    'details.apps.DetailsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- portfolioenv/portfolio-project/portfolio/urls.py
+++ portfolioenv/portfolio-project/portfolio/urls.py
@@ -18,12 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import friends.views
+import details.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('detail/',details.views.detail),
     path('', friends.views.home, name = "home"),
-    path('blog/', include('blog.urls')),
-
 ] + static(settings.MEDIA_URL,
 	   document_root = settings.MEDIA_ROOT)
 
