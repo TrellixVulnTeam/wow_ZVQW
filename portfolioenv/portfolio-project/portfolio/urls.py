47e85338e4c999b8f5c15ef2196bce18fdadad7e
이정민 <asd45kj@gmail.com>
2019-12-17 20:47:09 +0900

    friends -> home

--- portfolioenv/portfolio-project/portfolio/urls.py
+++ portfolioenv/portfolio-project/portfolio/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import friends.views
+import home.views
 import details.views
 import dinolists.views
 
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('list/',dinolists.views.dlist),
     path('detail/<name>',details.views.detail),
-    path('', friends.views.home, name = "home"),
+    path('', home.views.home, name = "home"),
     
 ] + static(settings.MEDIA_URL,
 	   document_root = settings.MEDIA_ROOT)
